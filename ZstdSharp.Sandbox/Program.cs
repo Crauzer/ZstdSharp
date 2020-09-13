179e27c8d49ba240057e7af199f2d5c4711f286d
Crauzer <filip.quitko@gmail.com>
2020-09-13 16:19:23 +0200

    sandbox stuff

--- ZstdSharp.Sandbox/Program.cs
+++ ZstdSharp.Sandbox/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace ZstdSharp.Sandbox
@@ -8,7 +9,30 @@ namespace ZstdSharp.Sandbox
     {
         static void Main(string[] args)
         {
+            using (MemoryStream compressed = new MemoryStream())
+            {
+                byte[] uncompressedArr = File.ReadAllBytes("mozilla");
 
+                using (ZstdStream zstdStream = new ZstdStream(compressed, ZstdStreamMode.Compress, leaveOpen: true))
+                {
+                    zstdStream.Write(uncompressedArr, 0, uncompressedArr.Length);
+                }
+
+                File.WriteAllBytes("mozilla2_zstd", Zstd.Decompress(compressed.ToArray(), uncompressedArr.Length));
+
+                using (MemoryStream uncompressed = new MemoryStream())
+                {
+                    compressed.Position = 0;
+
+                    using (ZstdStream zstdStream = new ZstdStream(compressed, ZstdStreamMode.Decompress))
+                    {
+                        zstdStream.CopyTo(uncompressed);
+                    }
+
+                    File.WriteAllBytes("mozilla2", uncompressed.ToArray());
+                }
+                
+            }
         }
     }
 }
