4ef3b179542e6f40afc17a860215a8f58d91eaf8
Crauzer <filip.quitko@gmail.com>
2020-09-13 16:04:05 +0200

    add support for mac and linux

--- ZstdSharp/Native.cs
+++ ZstdSharp/Native.cs
@@ -19,38 +19,6 @@ namespace ZstdSharp
 
         private const string ZSTD_DLL = "libzstd";
 
-        static Native()
-        {
-            string assemblyLocation = Path.GetDirectoryName(typeof(Native).Assembly.Location);
-            string architecture = Environment.Is64BitProcess ? "x64" : "x86";
-
-            SetDllDirectory(Path.Combine(assemblyLocation, architecture));
-        }
-
-        #region WinApi
-        [Flags]
-        private enum LoadLibraryFlags : uint
-        {
-            DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
-            LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
-            LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
-            LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
-            LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
-            LOAD_LIBRARY_SEARCH_APPLICATION_DIR = 0x00000200,
-            LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000,
-            LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
-            LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
-            LOAD_LIBRARY_SEARCH_USER_DIRS = 0x00000400,
-            LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
-        }
-
-        [DllImport("kernel32", SetLastError = true)]
-        private static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
-
-        [DllImport("kernel32", CharSet = CharSet.Auto)]
-        private static extern bool SetDllDirectory(string lpPathName);
-        #endregion
-
         #region Simple API
         [DllImport(ZSTD_DLL)]
         internal static extern Size ZSTD_compress(IntPtr destination, Size destinationCapacity, IntPtr source, Size sourceSize, int compressionLevel);
